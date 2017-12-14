d3458a0ea508b92ff05a691709483e05721b6a65
Parker McCurley <parker@decentcrypto.com>
2017-12-14 17:12:16 -0500

    nuke src

--- src/Greeter.test.js
+++
@@ -1,6 +0,0 @@
-import Greeter from "./Greeter";
-
-test("Greeter.greet", () => {
-  const greeter = new Greeter("test");
-  expect(greeter.greet()).toBe("test");
-});
--- src/index.js
+++
@@ -1,5 +0,0 @@
-import Greeter from "./Greeter";
-
-const greeter = new Greeter("Hello, world!");
-
-console.log(greeter.greet());
